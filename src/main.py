0869df8efb062904dd8294bee154138b256211ff
txemac <txema.bermudez@gmail.com>
2021-11-14 01:07:41 +0100

    add log file

--- src/main.py
+++ src/main.py
@@ -38,4 +38,4 @@ if __name__ == "__main__":
     import uvicorn
 
     app.debug = True
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_config="log.ini")
